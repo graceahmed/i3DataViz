a2503ef1ab01ccd9800ab687208e9297e4945a33
Grace Ahmed <guckele@uvm.edu>
2015-07-02 01:48:16 -0400

    Update more_better_figures.py

--- more_better_figures.py
+++ more_better_figures.py
@@ -17,14 +17,14 @@ bluecol   = "#599AD3"
 
 
 rects1 = plt.bar(ind, menMeans, width, yerr=menStd,
-                 error_kw={"ecolor":orangecol,"lw":4, 
-                           "capsize":8,"capthick":3},
-                 color=orangecol, lw=0, alpha=0.5)
+  error_kw={"ecolor":orangecol,"lw":4, 
+    "capsize":8,"capthick":3},
+  color=orangecol, lw=0, alpha=0.5)
 
 rects2 = plt.bar(ind-width, womenMeans, width, yerr=womenStd,
-                 error_kw={"ecolor":bluecol,"lw":4, 
-                           "capsize":8,"capthick":3},
-                 color=bluecol, lw=0, alpha=0.5)
+  error_kw={"ecolor":bluecol,"lw":4, 
+    "capsize":8,"capthick":3},
+  color=bluecol, lw=0, alpha=0.5)
 
 plt.title('Scores by group and gender', fontsize= 24)
 
@@ -33,7 +33,7 @@ plt.ylabel('Scores', fontsize = 18)
 plt.xticks(ind, ('1', '2', '3', '4', '5') )
 
 plt.legend( (rects1[0], rects2[0]), ('Men', 'Women'), 
-            fontsize=18, frameon=False )
+    fontsize=18, frameon=False )
 
 # add space to left and right:
 plt.xlim([0-2*width,4+2*width])
